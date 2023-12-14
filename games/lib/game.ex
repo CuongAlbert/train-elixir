877b77d934480c5b3f723d678ba96a85af2b39c0
CuongAlbert <cuongnd1710@gmail.comt>
2023-12-14 09:26:26 +0700

    finish Games: Rock Paper

--- games/lib/game.ex
+++ games/lib/game.ex
@@ -25,3 +25,22 @@ defmodule Games.GuessingGame do
     end
   end
 end
+
+defmodule Games.RockPaperScissors do
+  def play do
+    ai_choice = Enum.random(["rock", "paper", "scissors"])
+    IO.puts("AI choice: #{ai_choice}")
+    IO.puts("Choose rock, paper, or scissors:")
+    user_choice = IO.gets("") |> String.trim()
+
+    outcome = case {user_choice, ai_choice} do
+      {"rock", "scissors"} -> "You win! Rock beats scissors."
+      {"paper", "rock"} -> "You win! Paper beats rock."
+      {"scissors", "paper"} -> "You win! Scissors beats paper."
+      {choice, choice} -> "It's a tie!"
+      {_, _} -> "You lose! #{ai_choice} beats #{user_choice}."
+    end
+
+    IO.puts(outcome)
+  end
+end
