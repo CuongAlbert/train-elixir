28bccab7c6994e43225896366baeada6d7fc2ed1
CuongAlbert <cuongnd1710@gmail.comt>
2023-12-14 08:51:58 +0700

    finish Games: Guess

--- games/lib/games.ex
+++ games/lib/games.ex
@@ -1,18 +1,27 @@
-defmodule Games do
-  @moduledoc """
-  Documentation for `Games`.
-  """
+defmodule Games.GuessingGame do
+  def play do
+    target = Enum.random(1..10)
+    guess_loop(target, true, 5)
+  end
 
-  @doc """
-  Hello world.
+  defp guess_loop(target, is_first_loop, remaining) do
+    if remaining == 0 do
+      IO.puts("You lose! The answer was #{target}")
+    else
+      message = if is_first_loop, do: "Guess a number between 1 and 10:", else: "Enter your guess:"
+      IO.puts(message)
 
-  ## Examples
+      guess = IO.gets("") |> String.trim() |> String.to_integer()
 
-      iex> Games.hello()
-      :world
+      case guess do
+        ^target ->
+          IO.puts("Correct!")
 
-  """
-  def hello do
-    :world
+        _ ->
+          feedback = if guess < target, do: "Too Low!", else: "Too High!"
+          IO.puts(feedback)
+          guess_loop(target, false, remaining - 1)
+      end
+    end
   end
 end
