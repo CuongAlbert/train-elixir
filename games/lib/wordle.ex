d1d0d0c230ff70bb1d077cb27e625864f71da135
CuongAlbert <cuongnd1710@gmail.comt>
2023-12-15 11:46:24 +0700

    finish Typespec Drills exercise

--- games/lib/wordle.ex
+++ games/lib/wordle.ex
@@ -4,7 +4,20 @@ defmodule Games.Wordle do
     guess = IO.gets("Enter a five letter word:") |> String.trim()
     feedback(secret_word, guess)
   end
+@doc """
+iex> Games.Wordle.feedback("aaaaa", "aaaaa")
+[:green, :green, :green, :green, :green]
 
+iex> Games.Wordle.feedback("aaaaa", "aaaab")
+[:green, :green, :green, :green, :grey]
+
+iex> Games.Wordle.feedback("abdce", "edcba")
+[:yellow, :yellow, :yellow, :yellow, :yellow]
+
+# If There Are Duplicate Characters In The Guess Prioritize Exact Matches.
+iex> Games.Wordle.feedback("aaabb", "xaaaa")
+[:grey, :green, :green, :yellow, :yellow]
+"""
   def feedback(secret_word, guess) do
     Enum.zip(String.codepoints(secret_word), String.codepoints(guess))
     |> Enum.map(fn {s, g} ->
